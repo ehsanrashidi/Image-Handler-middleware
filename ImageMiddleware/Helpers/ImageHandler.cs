d3469f702bd4fd987869b6979f127f447dc07747
ehsanrashidi <ehsan.rashidi2001@gmail.com>
2023-07-29 00:22:02 +0330

    Ehsan

---
+++ ImageMiddleware/Helpers/ImageHandler.cs
@@ -0,0 +1,71 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ImageResizer.Helpers
+{
+    public class ImageHandler
+    {
+        public static byte[] CropImage(byte[] imageData, int x, int y, int width, int height)
+        {
+            using (MemoryStream ms = new MemoryStream(imageData))
+            {
+                using (System.Drawing.Image img = System.Drawing.Image.FromStream(ms))
+                {
+                    using (Bitmap bmp = new Bitmap(width, height))
+                    {
+                        bmp.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+
+                        using (Graphics g = Graphics.FromImage(bmp))
+                        {
+                            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                            g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+
+                            System.Drawing.Rectangle cropArea = new System.Drawing.Rectangle(x, y, width, height);
+                            g.DrawImage(img, new System.Drawing.Rectangle(0, 0, width, height), cropArea, GraphicsUnit.Pixel);
+                        }
+
+                        using (MemoryStream ms2 = new MemoryStream())
+                        {
+                            bmp.Save(ms2, ImageFormat.Jpeg);
+                            return ms2.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+
+        public static byte[] ResizeImage(byte[] imageData, int width, int height)
+        {
+            // Read the image data from the file into a byte array
+
+            // Create a new Bitmap object from the image data
+            using (MemoryStream ms = new MemoryStream(imageData))
+            {
+                using (Bitmap bmp = new Bitmap(ms))
+                {
+                    // Create a new Bitmap object with the desired width and height
+                    Bitmap resizedBmp = new Bitmap(width, height);
+
+                    // Create a Graphics object to draw the resized image
+                    using (Graphics g = Graphics.FromImage(resizedBmp))
+                    {
+                        // Set the interpolation mode to high quality bilinear for better image quality
+                        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+
+                        // Draw the resized image
+                        g.DrawImage(bmp, 0, 0, width, height);
+                    }
+
+                    // Save the resized image to a new byte array in the same format as the original image
+                    using (MemoryStream ms2 = new MemoryStream())
+                    {
+                        resizedBmp.Save(ms2, bmp.RawFormat);
+                        return ms2.ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ImageMiddleware/Middleware/ImageMiddleware.cs
@@ -0,0 +1,73 @@
+﻿using ImageResizer.Helpers;
+
+namespace ImageResizer.Middleware
+{
+    public class ImageMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ImageMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            // Check if the request path is for a static image file
+            if (context.Request.Path.Value.StartsWith("/Images/") && context.Request.Path.Value.EndsWith(".jpg"))
+            {
+
+                string widthQuery = context.Request.Query["width"];
+                string heightQuery = context.Request.Query["height"];
+                string operation = context.Request.Query["x"];
+
+                // Get the file path from the request path
+                string filePath = Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "wwwroot",
+                    context.Request.Path.Value.TrimStart('/'));
+
+                // Check if the file exists
+                if (File.Exists(filePath))
+                {
+
+                    // Read the image data from disk
+                    byte[] imageData = File.ReadAllBytes(filePath);
+
+                    // Crop the image
+                    int x = 0; // x-coordinate of top-left corner of crop area
+                    int y = 0; // y-coordinate of top-left corner of crop area
+
+                    // width of crop area
+                    int.TryParse(widthQuery, out int width);
+                    if (width == 0) width = 100;
+
+                    // height of crop area
+                    int.TryParse(heightQuery, out int height);
+                    if (height == 0) height = 100;
+                    context.Response.ContentType = "image/jpeg";
+
+                    if (operation == "crop")
+                    {
+                        byte[] croppedImageData = ImageHandler.CropImage(imageData, x, y, width, height);
+
+                        // Write the cropped image data to the response stream
+                        context.Response.ContentLength = croppedImageData.Length;
+                        await context.Response.Body.WriteAsync(croppedImageData, 0, croppedImageData.Length);
+                    }
+                    else
+                    {
+                        // Write the resized image data to the response stream
+                        byte[] resizedImageData = ImageHandler.ResizeImage(imageData, width, height);
+                        context.Response.ContentLength = resizedImageData.Length;
+                        await context.Response.Body.WriteAsync(resizedImageData, 0, resizedImageData.Length);
+
+                    }
+                    return;
+                }
+            }
+
+            // If the request is not for a static image file, pass it to the next middleware
+            await _next(context);
+        }
+    }
+}
